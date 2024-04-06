f172985b442bdeca0e8ccaa22c244d7649676419
akila514 <akilajay514@gmail.com>
2024-04-06 10:27:57 +0530

    Added form testing

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,5 +4,26 @@ describe("form tests", () => {
   });
   it("Test subscribe form", () => {
     cy.contains(/Testing Forms/i);
+
+    cy.getDataTest("subscribe-form").find("input").as("subscribe-input");
+
+    cy.get("@subscribe-input").type("test@gmail.com");
+    cy.contains(/Successfully subbed: test@gmail.com!/i).should("not.exist");
+
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/Successfully subbed: test@gmail.com!/i).should("exist");
+
+    cy.wait(3000);
+
+    cy.contains(/Successfully subbed: test@gmail.com!/i).should("not.exist");
+
+    cy.get("@subscribe-input").type("test@code.io");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/Invalid email: test@code.io!/i).should("exist");
+
+    cy.wait(3000);
+
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/fail!/i).should("exist");
   });
 });
